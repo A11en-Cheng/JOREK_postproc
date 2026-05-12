9e8cfb959679bbfcb8b8257511a96091ddb9e6e2
Allen Cheng <Allencheng@buaa.edu.cn>
2026-05-12 19:01:21 +0800

    fix: 修正测试工作流中pytest的调用方式，确保正确执行测试 fix: 更新setup.py中的README路径，确保正确读取长描述

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ if not hasattr(configparser, 'SafeConfigParser'):
 
 from setuptools import setup, find_packages
 
-with open("jorek_postproc/README.md", "r", encoding="utf-8") as fh:
+with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
